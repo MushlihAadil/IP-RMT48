346280407a961abaa419c7df0eda0de4c33a2342
Muhammad Mushlih Al 'Aadil <mmushlih.alaadil@gmail.com>
2024-05-17 11:33:39 +0700

    client-update

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,6 +1,15 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../style/styleHome.css";
 
 export const BookCard = ({ book }) => {
+    const navigate = useNavigate();
+    const [favourite, setFavourite] = useState([]);
+
+    const handleAddFavourite = async (title, ) => {
+
+    }
+
   return (
     <>
         <div className="card">
@@ -15,8 +24,9 @@ export const BookCard = ({ book }) => {
             <h5 className="card-title"> Author : {book.author} </h5>
             <h5 className="card-title"> Release : {book.releaseDate} </h5>
             <h5 className="card-title"> Total Pages : {book.pages} </h5>
+            <h6 className="card-title"> Description : {book.description} </h6>
             <h5 className="card-title"> Book Price : Rp. {book.price} </h5>
-            <button className="btn btn-success">Fav</button>
+            <button className="btn" onClick="">Fav</button>
           </div>
         </div>
     </>
