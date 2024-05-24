f1f2c12083d90f88b7de32b325e4fcf859243e3b
Muhammad Mushlih Al 'Aadil <mmushlih.alaadil@gmail.com>
2024-05-24 09:19:19 +0700

    server update deletions v5

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,7 +1,19 @@
-const { Favourite } = require("../models");
+const { verifyToken } = require("../helper/jwt");
+const { Favourite, User } = require("../models");
 
 const authorization = async (req, res, next) => {
   try {
+    let access_token = req.headers.authorization;
+    if (!access_token) throw { name: "InvalidToken" };
+
+    let tokenSplit = access_token.split(" ");
+    let [bearer, token] = tokenSplit;
+    if (bearer !== "Bearer") throw { name: "InvalidToken" };
+
+    let payload = verifyToken(token);
+    let user = await User.findByPk(payload.id);
+    if (!user) throw { name: "InvalidToken" };
+
     const { bookId } = req.params;
     let favourite = await Favourite.findOne({
       where: {
@@ -10,9 +22,9 @@ const authorization = async (req, res, next) => {
     });
     if (!favourite) throw { name: "FavouriteNotFound" };
 
-    if (favourite.userId == req.user.id) {
+    if (favourite.userId == user.id) {
       next();
-    } else if (favourite.userId != req.user.id) {
+    } else if (favourite.userId != user.id) {
       throw { name: "Unauthorized" };
     }
   } catch (err) {
